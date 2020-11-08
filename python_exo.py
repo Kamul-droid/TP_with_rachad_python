ba84691b6561b96ceadcbcc96e0c456995c0433c
Kamul <kamul.alinassomawattara@supinfo.com>
2020-11-08 09:40:14 +0100

    tva_commit1

---
+++ python_exo.py
@@ -0,0 +1,17 @@
+#Algorithme
+// on demande à l'utilisateur de rentrer les informations
+
+prix = int(input("Entrer le prix unitaire"))
+
+Nombre_article=int(input("Entrer le nombre d'articles"))
+
+TVA = int(input("Entrer la TVA"))
+
+	
+// on effectue le calcul
+	
+montant_ttc = (prix*Nombre_article)(1+TVA)
+	
+// on affiche le résultat
+	
+print("le montant TTC de l'achat est",montant_ttc)
